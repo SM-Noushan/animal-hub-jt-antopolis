35609fded49d447c8f6aa96e654c76f14e50edfb
SMNoushan <noushan.md@gmail.com>
2024-08-23 07:32:12 +0600

    added suspense boundary

--- src/app/page.js
+++ src/app/page.js
@@ -2,20 +2,23 @@ import { getAllCategories } from "@/api";
 import Add from "@/components/addAnimalsOrCategories/Add";
 import Categories from "@/components/categories/Categories";
 import AnimalsData from "@/components/animalsData/AnimalsData";
+import { Suspense } from "react";
 
 export default async function Home() {
   const categories = await getAllCategories();
   return (
-    <main className="min-h-screen max-w-screen-2xl container mx-auto px-4 py-8 lg:p-16 xl:p-20">
-      <div className="flex flex-col-reverse lg:flex-row justify-between gap-8 lg:gap-12">
-        {/* categories */}
-        <Categories categories={categories} />
+    <Suspense>
+      <main className="min-h-screen max-w-screen-2xl container mx-auto px-4 py-8 lg:p-16 xl:p-20">
+        <div className="flex flex-col-reverse lg:flex-row justify-between gap-8 lg:gap-12">
+          {/* categories */}
+          <Categories categories={categories} />
 
-        {/* add animals or category */}
-        <Add categories={categories} />
-      </div>
-      {/* animals data */}
-      <AnimalsData />
-    </main>
+          {/* add animals or category */}
+          <Add categories={categories} />
+        </div>
+        {/* animals data */}
+        <AnimalsData />
+      </main>
+    </Suspense>
   );
 }
